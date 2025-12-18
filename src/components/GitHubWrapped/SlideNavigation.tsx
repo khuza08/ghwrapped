f4c61be0f64cbb8b2be883a6c50d4a7ed08ecdfc
huza <huzaifahasif3@gmail.com>
2025-12-18 21:16:39 +0700

    adjust navigation pills

--- src/components/GitHubWrapped/SlideNavigation.tsx
+++ src/components/GitHubWrapped/SlideNavigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 interface SlideNavigationProps {
   currentSlide: number;
@@ -22,20 +23,21 @@ const SlideNavigation: React.FC<SlideNavigationProps> = ({
   return (
     <div className="fixed bottom-0 left-0 right-0 p-4 z-20">
       <div className="max-w-6xl mx-auto px-6 py-6 ">
-        <div className="flex flex-row items-center justify-between gap-3">
+        <div className="flex flex-row items-center justify-center gap-2">
           <button
             onClick={onPrev}
             disabled={currentSlide === 0}
-            className={`px-4 py-2 backdrop-blur-xl ${
+            className={`p-3 backdrop-blur-xl ${
               currentSlide === 0
-                ? "bg-white/5 rounded-full border border-white/20 py-2 px-6 text-white/50 cursor-not-allowed"
-                : "bg-white/10 rounded-full border border-white/20 py-2 px-6 text-white hover:bg-white/5"
+                ? "bg-white/5 rounded-full border border-white/20 text-white/50 cursor-not-allowed"
+                : "bg-white/10 rounded-full border border-white/20 text-white hover:bg-white/5"
             }`}
+            aria-label="Previous slide"
           >
-            Previous
+            <IoIosArrowBack className="w-6 h-6" />
           </button>
 
-          <div className="flex space-x-4 px-4 py-4 bg-white/5 border border-white/20 rounded-full backdrop-blur-xl ">
+          <div className="flex space-x-3 px-4 py-4 bg-white/5 border border-white/20 rounded-full backdrop-blur-xl">
             {slides.map((_, index) => (
               <button
                 key={index}
@@ -51,13 +53,14 @@ const SlideNavigation: React.FC<SlideNavigationProps> = ({
           <button
             onClick={onNext}
             disabled={currentSlide === totalSlides - 1}
-            className={`px-4 py-2 backdrop-blur-xl ${
+            className={`p-3 backdrop-blur-xl ${
               currentSlide === totalSlides - 1
-                ? "bg-white/20 rounded-full border border-white/20 py-2 px-6 text-white/50 cursor-not-allowed"
-                : "bg-white/10 rounded-full border border-white/20 py-2 px-6 text-white hover:bg-white/5 transition"
+                ? "bg-white/20 rounded-full border border-white/20 text-white/50 cursor-not-allowed"
+                : "bg-white/10 rounded-full border border-white/20 text-white hover:bg-white/5 transition"
             }`}
+            aria-label="Next slide"
           >
-            Next
+            <IoIosArrowForward className="w-6 h-6" />
           </button>
         </div>
       </div>
