266ad7199723fb90808877ddd2ff12d39e113e71
huza <huzaifahasif3@gmail.com>
2025-12-21 18:16:26 +0700

    refine footer

--- src/components/UI/GitHubWrappedFooter.tsx
+++ src/components/UI/GitHubWrappedFooter.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const GitHubWrappedFooter: React.FC = () => {
   return (
-    <footer className="mt-8 sm:mt-12 text-center text-white/80 text-xs sm:text-sm">
+    <footer className="mb-6 text-center text-white/80 text-xs sm:text-sm">
       <p>
-        yet another simple GitHub Wrapped by elza; This project uses only public
+        yet another simple GitHub Wrapped by elza; This project uses only{" "}
+        <strong className="font-black"> public </strong>
         GitHub data.
       </p>
     </footer>
